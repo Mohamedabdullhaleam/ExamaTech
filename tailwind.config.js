bd1f7cabbabc1e4f591a8515abd0495b9e761efe
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-01-21 13:22:47 +0200

    setup: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      //// any color or font extensions
+    },
+  },
+  plugins: [],
+};
