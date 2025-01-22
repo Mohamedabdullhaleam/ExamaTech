9a5c2d83658eeab39f745cc856a568e46e75841a
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-01-22 23:08:50 +0200

    feat:dynamic and generic error msg

--- js/pages/signUp.js
+++ js/pages/signUp.js
@@ -71,13 +71,15 @@ async function validate(event) {
 function displyError(errors) {
   // 1- clear all previous errors
   document.querySelectorAll(".text-red-500").forEach(function (errorElement) {
-    errorElement.textContent = "";
+    errorElement.textContent = "hhh";
+    // errorElement.classList.remove("visible");
     errorElement.classList.add("invisible");
   });
   // 2- display new errors
   errors.forEach(function (error) {
     const errorElement = document.getElementById(error.id);
     errorElement.textContent = error.message;
+    // errorElement.classList.add("visible");
     errorElement.classList.remove("invisible");
   });
 }
