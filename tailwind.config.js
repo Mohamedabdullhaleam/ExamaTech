10f1d6a1d3920c69ca4e38df058079320e1e5cd0
KholoudELkasaby <lilelksaby@gmail.com>
2025-01-24 00:31:49 +0200

    feat:static pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      //// any color or font extensions
+      colors: {
+        "main-color": "#8FA38B",
+        "sec-color": "#556352",
+        "text-color": "#180F11",
+        "light-color": "#8E8C92",
+        "light-green": "#EBEBEB",
+        "Btn-color": "#73826F",
+      },
     },
   },
   plugins: [],
