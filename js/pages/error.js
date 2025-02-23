1865a6e6a59ca3ccf8d42163bd57c68caf0c82d1
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-02-23 14:35:56 +0200

    delete

--- js/pages/error.js
+++ js/pages/error.js
@@ -3,10 +3,3 @@ import { displayUserNameWithEffect } from "../utils/textAnimation.js";
 / * * * Animation * * * /;
 const title = document.getElementById("title");
 displayUserNameWithEffect(title, "Exama-Tech");
-
-/ * * * Redirection * * * /;
-const login = document.getElementById("login");
-login.addEventListener("click", () => {
-  localStorage.clear();
-  window.location.replace("SignIn.html");
-});
